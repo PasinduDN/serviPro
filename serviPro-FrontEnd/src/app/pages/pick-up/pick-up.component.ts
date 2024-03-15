4bbaa6f18fc3c060d8259039a76ffce1feb79f42
Pasindu Dulanjana <pasindusl07@gmail.com>
2024-03-15 17:48:05 +0530

    Update PickUp Component

--- serviPro-FrontEnd/src/app/pages/pick-up/pick-up.component.ts
+++ serviPro-FrontEnd/src/app/pages/pick-up/pick-up.component.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-pick-up',
@@ -14,6 +18,7 @@ export class PickUpComponent implements OnInit{
   public item:any;
   public filterdItemArray : Object [] = [];
   public loadItemArray : any [] = [];
+  public total = 0;
 
   //col-3
   public selectedItem:any [] = [];
@@ -21,7 +26,7 @@ export class PickUpComponent implements OnInit{
   public selectCatgory:any;
   public itemForTempSave:any;
 
-  constructor (private httpCLient: HttpClient){
+  constructor (private httpCLient: HttpClient,){
     this.http=httpCLient;
   }
 
@@ -46,15 +51,6 @@ export class PickUpComponent implements OnInit{
         this.itemList=data;
       })
   }
-
-  // public selectItem = {
-  //   itemid: null,
-  //   itemCode: null,
-  //   itemName: null,
-  //   category: null,
-  //   categoryId:null,
-  //   itemPrice: null
-  // }
 
   selectedCategory(category:any){
     console.log(category);
@@ -94,12 +90,12 @@ export class PickUpComponent implements OnInit{
             itemqty: 1,
             itemName: this.loadItemArray[i].itemName,
             itmDiscount: 0,
-            itemPrice: this.loadItemArray[i].itemPrice
+            itemPrice: this.loadItemArray[i].itemPrice,
+            // totalPrice: this.loadItemArray[i].itemPrice
           };
-
           this.selectedItem.push(newItem); 
-
-          console.log(itemName);
+          this.calculateTotal();
+          console.log("what "+ itemName);
         }
       }
   }
@@ -108,31 +104,79 @@ export class PickUpComponent implements OnInit{
     this.selectedItem=[];
   }
 
-  cleckItemforTempSave(item:any){
+  clickItemforTempSave(item:any){
     console.log("cleckItemforTempSave" +item.itemPrice );
     this.itemForTempSave = item;
   }
 
   clickToUpdateItem(){
-    for(let i=0; i<this.selectedItem.length; i++){
-      
-      if(this.itemForTempSave.itemid == this.selectedItem[i].itemid){
+    let length = this.selectedItem.length;
+    console.log("Update Function Start")
+    for(let i=0; i<length; i++){
+      if(this.selectedItem[i].itemid == this.itemForTempSave.itemid){
+        console.log("If Start")
+        this.selectedItem[i].itemid = this.itemForTempSave.itemid;
         this.selectedItem[i].itemqty = this.itemForTempSave.itemqty;
+        this.selectedItem[i].itemName = this.itemForTempSave.itemName;
         this.selectedItem[i].itmDiscount = this.itemForTempSave.itmDiscount;
         this.selectedItem[i].itemPrice = this.itemForTempSave.itemPrice;
-        let newItem = {
-          itemid: this.loadItemArray[i].itemid,
-          itemqty: 1,
-          itemName: this.loadItemArray[i].itemName,
-          itmDiscount: 0,
-          itemPrice: this.loadItemArray[i].itemPrice
-        };
+
+        if(this.itemForTempSave.itmDiscount >0){
+          console.log("itmDiscount > 0 Start");
+          this.selectedItem[i].itemPrice = this.selectedItem[i].itemPrice - (this.selectedItem[i].itemPrice * (this.itemForTempSave.itmDiscount/100));
+        }else if (this.itemForTempSave.itemqty > 1){
+          this.selectedItem[i].itemPrice = (this.itemForTempSave.itemqty * this.itemForTempSave.itemPrice);
+          console.log("itemqty > 1 Start")
+        }else{
+          this.selectedItem[i].itemPrice = this.itemForTempSave.itemPrice;
+        }
       }
     }
+
+    this.calculateTotal();
   }
 
   deleteItem(){
 
+    let tempArray  : any [] = [];
+
+    let length = this.selectedItem.length;
+    console.log("Length - " +this.selectedItem.length )
+    console.log("tempSave" + this.itemForTempSave.itemid)
+
+    for(let i=0; i<length; i++){
+      if(this.selectedItem[i].itemid != this.itemForTempSave.itemid){
+
+        let newItem = {
+                itemid: this.selectedItem[i].itemid,
+                itemqty: this.selectedItem[i].itemqty,
+                itemName: this.selectedItem[i].itemName,
+                itmDiscount: this.selectedItem[i].itmDiscount,
+                itemPrice: this.selectedItem[i].itemPrice
+        };
+        tempArray.push(newItem); 
+        console.log("Loop without id - " +this.selectedItem[i].itemid )
+      }
+    }
+
+    this.selectedItem=tempArray;
+    
+  }
+
+  calculateTotal(){
+    this.total = 0;
+    let length = this.selectedItem.length;
+    for(let i=0; i<length; i++){
+      this.total = this.total + this.selectedItem[i].itemPrice;
+    }
+  }
+
+  orderSave(){
+    this.http 
+      .post('http://localhost:8080/item/getItemList',this.selectCatgory)
+      .subscribe((data: any) => {
+        console.log(data);
+      });
   }
 
 }
