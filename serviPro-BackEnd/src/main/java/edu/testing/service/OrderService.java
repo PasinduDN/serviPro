c805541fbf282d4f2c105020e4a5e238374c58c1
Pasindu Dulanjana <pasindusl07@gmail.com>
2024-03-15 17:51:52 +0530

    Create Place Order Section

---
+++ serviPro-BackEnd/src/main/java/edu/testing/service/OrderService.java
@@ -0,0 +1,4 @@
+package edu.testing.service;
+
+public interface OrderService {
+}
---
+++ serviPro-BackEnd/src/main/java/edu/testing/service/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package edu.testing.service.impl;
+
+import edu.testing.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
